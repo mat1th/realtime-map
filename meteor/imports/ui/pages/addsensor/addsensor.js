5dabe2b753af98112e5f98cafcf9db5877ec838f
Matth96 <Matthias.Dolstra@hva.nl>
2016-04-25 21:23:16 +0200

    add form to add new sensor.

--- meteor/imports/ui/pages/addsensor/addsensor.js
+++ meteor/imports/ui/pages/addsensor/addsensor.js
@@ -1,1 +1,10 @@
 import './addsensor.html';
+import {
+    Template
+} from 'meteor/templating';
+
+Template.myTemplate.events({
+    'click button': function(event) {
+      console.log(event);
+    }
+});
--- meteor/server/methods.js
+++ meteor/server/methods.js
@@ -1,5 +1,12 @@
 'use strict';
+import { check } from 'meteor/check'
 
 Meteor.methods({
-  
+  foo: function (name, location, gps) {
+    check(name, String);
+    check(location, String);
+
+
+    return "some return value";
+  }
 });
