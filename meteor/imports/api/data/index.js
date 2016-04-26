3450d58a2cf184ac6b529ed77a0ae0cac303f3c2
Matth96 <Matthias.Dolstra@hva.nl>
2016-04-26 13:39:52 +0200

    fix api

--- meteor/imports/api/data/index.js
+++ meteor/imports/api/data/index.js
@@ -6,52 +6,55 @@ var sensorsValue = {
 }
 
 export const database = (function() {
-            function set(postData) {
-                var now = moment().format('YYYY-MM-DD HH:mm:ss');
+    function set(postData) {
+        var now = moment().format('YYYY-MM-DD HH:mm:ss');
 
-                sensorsValue.sensor = sensorsValue.sensor || postData.sensor;
-                sensorsValue.sensorId = sensorsValue.sensorId || postData.sensorId;
+        sensorsValue.sensor = sensorsValue.sensor || postData.sensor;
+        sensorsValue.sensorId = sensorsValue.sensorId || postData.sensorId;
 
-                if (sensorsValue.sensor && sensorsValue.sensorId) {
-                        SensorData.insert({
-                            date: now,
-                            sensorId: sensorsValue.sensor,
-                            sensorvalue: sensorsValue.sensorId
-                        });
-                        sensorsValue = {
-                            sensor: undefined,
-                            sensorId: undefined
-                        }
-                    }
-                }
+        if (sensorsValue.sensor && sensorsValue.sensorId) {
+            SensorData.insert({
+                date: now,
+                sensorId: sensorsValue.sensorId,
+                sensorvalue: sensorsValue.sensor
+            });
+            sensorsValue = {
+                sensor: undefined,
+                sensorId: undefined
+            }
+        }
+    }
 
-                function getStatus() {
-                    var data = SensorData.find({}, {
-                        sort: {
-                            date: -1
-                        },
-                        limit: 1
-                    }).fetch({});
-                    var status = {
-                        date: data[0].date,
-                        sensorStatus: {
-                            sensor1: data[0].sensor1,
-                            sensor2: data[0].sensor2,
-                            sensor3: data[0].sensor3
-                        }
-                    }
-                    return JSON.stringify(status);
-                }
+    function getStatus() {
+        var data = SensorData.find({}, {
+            sort: {
+                date: -1
+            },
+            limit: 1
+        }).fetch({});
 
-                function get(limit) {
-                    return JSON.stringify(SensorData.find({}, {
-                        limit: limit
-                    }).fetch({}));
-                }
+        console.log(data);
 
-                return {
-                    set: set,
-                    get: get,
-                    getStatus: getStatus
-                };
-            })();
+        var status = {
+            date: data[0].date,
+            sensorStatus: {
+                date: data[0].date,
+                sensorId: data[0].sensorvalue,
+                sensorvalue: data[0].sensorvalue,
+            }
+        }
+        return JSON.stringify(status);
+    }
+
+    function get(limit) {
+        return JSON.stringify(SensorData.find({}, {
+            limit: limit
+        }).fetch({}));
+    }
+
+    return {
+        set: set,
+        get: get,
+        getStatus: getStatus
+    };
+})();
