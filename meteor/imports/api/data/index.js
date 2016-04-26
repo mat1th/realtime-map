3f705b01b817a0454fb39b35ded43afac5ba594a
Matth96 <Matthias.Dolstra@hva.nl>
2016-04-26 13:23:02 +0200

    add new api

--- meteor/imports/api/data/index.js
+++ meteor/imports/api/data/index.js
@@ -1,80 +1,57 @@
 'use strict';
 
 var sensorsValue = {
-    sensor1: undefined,
-    sensor1id: undefined,
-    sensor2: undefined,
-    sensor2id: undefined,
-    sensor3: undefined,
-    sensor3id: undefined
+    sensor: undefined,
+    sensorId: undefined
 }
 
 export const database = (function() {
-    function set(postData) {
-        var now = moment().format('YYYY-MM-DD HH:mm:ss');
+            function set(postData) {
+                var now = moment().format('YYYY-MM-DD HH:mm:ss');
 
-        sensorsValue.sensor1 = sensorsValue.sensor1 || postData.sensor1;
-        sensorsValue.sensor1id = sensorsValue.sensor1id || postData.sensor1id;
-        sensorsValue.sensor2 = sensorsValue.sensor2 || postData.sensor2;
-        sensorsValue.sensor2id = sensorsValue.sensor2id || postData.sensor2id;
-        sensorsValue.sensor3 = sensorsValue.sensor3 || postData.sensor3;
-        sensorsValue.sensor3id = sensorsValue.sensor3id || postData.sensor3id;
+                sensorsValue.sensor = sensorsValue.sensor || postData.sensor;
+                sensorsValue.sensorId = sensorsValue.sensorId || postData.sensorId;
 
-        if (sensorsValue.sensor1 && sensorsValue.sensor2 && sensorsValue.sensor3) {
-            SensorData.insert({
-                date: now,
-                sensor1: {
-                    id: sensorsValue.sensor1id,
-                    value: sensorsValue.sensor1
-                },
-                sensor2: {
-                    id: sensorsValue.sensor2id,
-                    value: sensorsValue.sensor2
-                },
-                sensor3: {
-                    id: sensorsValue.sensor3id,
-                    value: sensorsValue.sensor3
+                if (sensorsValue.sensor && sensorsValue.sensorId) {
+                        SensorData.insert({
+                            date: now,
+                            sensorId: sensorsValue.sensor,
+                            sensorvalue: sensorsValue.sensorId
+                        });
+                        sensorsValue = {
+                            sensor: undefined,
+                            sensorId: undefined
+                        }
+                    }
                 }
-            });
 
-            sensorsValue = {
-                sensor1: undefined,
-                sensor1id: undefined,
-                sensor2: undefined,
-                sensor2id: undefined,
-                sensor3: undefined,
-                sensor3id: undefined
-            };
-        }
-    }
+                function getStatus() {
+                    var data = SensorData.find({}, {
+                        sort: {
+                            date: -1
+                        },
+                        limit: 1
+                    }).fetch({});
+                    var status = {
+                        date: data[0].date,
+                        sensorStatus: {
+                            sensor1: data[0].sensor1,
+                            sensor2: data[0].sensor2,
+                            sensor3: data[0].sensor3
+                        }
+                    }
+                    return JSON.stringify(status);
+                }
 
-    function getStatus() {
-        var data = SensorData.find({}, {
-            sort: {
-                date: -1
-            },
-            limit: 1
-        }).fetch({});
-        var status = {
-            date: data[0].date,
-            sensorStatus: {
-                sensor1: data[0].sensor1,
-                sensor2: data[0].sensor2,
-                sensor3: data[0].sensor3
-            }
-        }
-        return JSON.stringify(status);
-    }
+                function get(limit) {
+                    return JSON.stringify(SensorData.find({}, {
+                        limit: limit
+                    }).fetch({}));
+                }
 
-    function get(limit) {
-        return JSON.stringify(SensorData.find({}, {
-            limit: limit
-        }).fetch({}));
-    }
-
-    return {
-        set: set,
-        get: get,
-        getStatus: getStatus
-    };
-})();
+                return {
+                    set: set,
+                    get: get,
+                    getStatus: getStatus
+                };
+            })();
--- meteor/lib/collections/sensorData.js
+++ meteor/lib/collections/sensorData.js
@@ -1,28 +1,15 @@
-
 SensorData = new Mongo.Collection('sensorData');
 
 SensorData.schema = new SimpleSchema({
     date: {
         type: String
     },
-    "sensor1.id": {
+    sensorvalue: {
         type: String
     },
-    "sensor1.value": {
+    sensorId: {
         type: String
-    },
-    "sensor2.id": {
-        type: String
-    },
-    "sensor2.value": {
-        type: String
-    },
-    "sensor3.id": {
-        type: String
-    },
-    "sensor3.value": {
-        type: String
-    },
+    }
 });
 
 SensorData.attachSchema(SensorData.schema);
