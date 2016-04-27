ae1475686554843ba9e10b2fc969e0b43703f7d4
Matth96 <Matthias.Dolstra@hva.nl>
2016-04-27 14:46:59 +0200

    add tooltip with real data

--- meteor/imports/ui/pages/chart/chart.js
+++ meteor/imports/ui/pages/chart/chart.js
@@ -13,7 +13,12 @@ Template.chart.rendered = function() {
 
 
     var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse,
-        formatDate = d3.time.format("%Y");
+        formatDate = d3.time.format("%Y"),
+        bisectDate = d3.bisector(function(d) {
+            return d.date;
+        }).left;
+
+
 
     var x = d3.time.scale()
         .range([0, width]);
@@ -67,6 +72,10 @@ Template.chart.rendered = function() {
         .attr("x2", "0%")
         .attr("y2", "100%");
 
+    var focus = svg.append("g")
+        .attr("id", "focus")
+        .style("display", "none");
+
     gradient.append("stop")
         .attr("offset", "0%")
         .attr("stop-color", "#64929d");
@@ -90,7 +99,7 @@ Template.chart.rendered = function() {
         .attr("transform", "translate(" + width + ",0)");
 
     svg.append("path")
-        .attr("class", "area graph")
+        .attr("class", "area")
         .attr("clip-path", "url(#clip)")
         .style("fill", "url(#gradient)");
 
@@ -99,7 +108,8 @@ Template.chart.rendered = function() {
         .attr("transform", "translate(0," + height + ")");
 
     svg.append("path")
-        .attr("class", "line")
+        .attr("class", "line graph")
+        .attr("style", "padding: 100px")
         .attr("clip-path", "url(#clip)");
     //
     // svg.append("rect")
@@ -111,8 +121,6 @@ Template.chart.rendered = function() {
     var data = SensorData.find({
         sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4"
     }).fetch({})
-
-    console.log(SensorData.find({}).fetch({}));
 
     data.forEach(function(d) {
         d.date = parseDate(d.date);
@@ -137,29 +145,123 @@ Template.chart.rendered = function() {
         svg.select("path.line").attr("d", line);
     }
 
-    var graph = svg.select(".graph");
-    graph.on("mouseout", mouseover)
-        .on("mousemove", mousemove)
-        .on("mouseout", mouseout);
+
+    // place the value at the intersection
+    focus.append("text")
+        .attr("class", "y1")
+        .style("stroke", "white")
+        .style("stroke-width", "3.5px")
+        .style("opacity", 0.8)
+        .attr("dx", 8)
+        .attr("dy", "-.3em");
+
+    focus.append("text")
+        .attr("class", "y2")
+        .attr("dx", 8)
+        .attr("dy", "-.3em");
+
+    // place the date at the intersection
+    focus.append("text")
+        .attr("class", "y3")
+        .style("stroke", "white")
+        .style("stroke-width", "3.5px")
+        .style("opacity", 0.8)
+        .attr("dx", 8)
+        .attr("dy", "1em");
+    focus.append("text")
+        .attr("class", "y4")
+        .attr("dx", 8)
+        .attr("dy", "1em");
+
+    svg.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "none")
+        .style("pointer-events", "all")
+        .on("mouseover", function() {
+            focus.style("display", null);
+        })
+        .on("mouseout", function() {
+            focus.style("display", "none");
+        })
+        .on("mousemove", mousemove);
+
 
     function mousemove() {
-        var div = d3.select(".tooltip");
-        var timeDiv = d3.select(".time");
-        var value = Math.round(y.invert(d3.mouse(this)[0]));
-        var timeValue = x.invert(d3.mouse(this)[0]);
-        console.log(timeValue);
-        // timeDiv.text(parseDate(timeValue));
-        div.select(".circle")
-            .text(value);
-        div.select(".groups")
-            .text("Groepen: 97%");
-        div.select(".sound")
-            .text("Geluidsoverlast: 97%");
-        div.select(".present")
-            .text("Aanwezig: 10%");
-        div.style("left", (d3.event.pageX + 0) + "px")
-            .style("top", (0) + "px");
+        var x0 = x.invert(d3.mouse(this)[0]),
+            i = bisectDate(data, x0, 1),
+            d0 = data[i - 1],
+            d1 = data[i],
+            d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+
+        console.log(d);
+        focus.select("circle.y")
+            .attr("transform",
+                "translate(" + x(d.date) + "," +
+                y(d.value) + ")");
+
+        focus.select("text.y1")
+            .attr("transform",
+                "translate(" + x(d.date) + "," +
+                y(d.value) + ")")
+            .text(d.value);
+
+        focus.select("text.y2")
+            .attr("transform",
+                "translate(" + x(d.date) + "," +
+                y(d.value) + ")")
+            .text(d.value);
+
+        focus.select("text.y3")
+            .attr("transform",
+                "translate(" + x(d.date) + "," +
+                y(d.value) + ")")
+            .text(formatDate(d.date));
+
+        focus.select("text.y4")
+            .attr("transform",
+                "translate(" + x(d.date) + "," +
+                y(d.value) + ")")
+            .text(formatDate(d.date));
+
+        focus.select(".x")
+            .attr("transform",
+                "translate(" + x(d.date) + "," +
+                y(d.value) + ")")
+            .attr("y2", height - y(d.value));
+
+        focus.select(".y")
+            .attr("transform",
+                "translate(" + width * -1 + "," +
+                y(d.value) + ")")
+            .attr("x2", width + width);
     }
+
+
+
+    // var graph = svg.select(".graph");
+    // graph.on("mouseout", mouseover)
+    //     .on("mousemove", mousemove)
+    //     .on("mouseout", mouseout);
+    //
+    // function mousemove() {
+    //     var div = d3.select(".tooltip");
+    //     var timeDiv = d3.select(".time");
+    //     var value = Math.round(y.invert(d3.mouse(this)[1]));
+    //     var timeValue = x.invert(d3.mouse(this)[0]);
+    //     console.log(y.invert(d3.mouse(this)[0]));
+    //     timeDiv.text(timeValue);
+    //     div.select(".circle")
+    //         .text(value);
+    //     div.select(".groups")
+    //         .text("Groepen: 97%");
+    //     div.select(".sound")
+    //         .text("Geluidsoverlast: 97%");
+    //     div.select(".present")
+    //         .text("Aanwezig: 10%");
+    //     div.style("left", (d3.event.pageX + 0) + "px")
+    //         .style("top", (0) + "px");
+    // }
 }
 
 
