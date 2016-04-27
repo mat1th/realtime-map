02382207c278bd6e2e15062ef3773d3e63978f5b
Fons Hettema <fons.hettema@hva.nl>
2016-04-27 14:00:25 +0200

    New data

--- meteor/server/server.js
+++ meteor/server/server.js
@@ -1,46 +1,46 @@
 import '../imports/startup/server/index.js'
 
 // Sensors.remove({});
-// SensorData.remove({});
+SensorData.remove({});
 //
 //
-// var data = [{
-//     _id: "MWhBKBTLFuo4TWbsdfsd",
-//     date: "2016-04-26 11:51:39",
-//     sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
-//     sensorvalue: "120"
-// }, {
-//     _id: "7QcNux234s2xvHDHgZ",
-//     date: "2016-04-10 11:51:40",
-//     sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
-//     sensorvalue: "389"
-// }, {
-//     _id: "FyJGRg242ew24k8poe",
-//     date: "2016-04-11 11:51:40",
-//     sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
-//     sensorvalue: "499"
-// }, {
-//     _id: "MwDuFaFt23423dDcX4R",
-//     date: "2016-04-14 11:51:40",
-//     sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
-//     sensorvalue: "324"
-// }, {
-//     _id: "MwDuFaFt42332DcX4R",
-//     date: "2016-04-12 11:51:40",
-//     sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
-//     sensorvalue: "123"
-// }, {
-//     _id: "MwDuFaFt23432dDcX4R",
-//     date: "2016-04-16 11:51:40",
-//     sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
-//     sensorvalue: "412"
-// }, {
-//     _id: "sd2dfasdfdasfdss33",
-//     date: "2016-04-20 11:51:40",
-//     sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
-//     sensorvalue: "421"
-// }];
-//
-// data.forEach(function(elemnt) {
-//     SensorData.insert(elemnt);
-// })
+var data = [{
+    _id: "MWhBKBTLFuo4TWbsdfsd",
+    date: "2016-04-10 11:51:39",
+    sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
+    sensorvalue: "100"
+}, {
+    _id: "7QcNux234s2xvHDHgZ",
+    date: "2016-04-12 11:51:40",
+    sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
+    sensorvalue: "120"
+}, {
+    _id: "FyJGRg242ew24k8poe",
+    date: "2016-04-14 11:51:40",
+    sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
+    sensorvalue: "110"
+}, {
+    _id: "MwDuFaFt23423dDcX4R",
+    date: "2016-04-16 11:51:40",
+    sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
+    sensorvalue: "130"
+}, {
+    _id: "MwDuFaFt42332DcX4R",
+    date: "2016-04-18 11:51:40",
+    sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
+    sensorvalue: "120"
+}, {
+    _id: "MwDuFaFt23432dDcX4R",
+    date: "2016-04-20 11:51:40",
+    sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
+    sensorvalue: "140"
+}, {
+    _id: "sd2dfasdfdasfdss33",
+    date: "2016-04-22 11:51:40",
+    sensorId: "53180077-cfc9-49b7-b807-ec01cd02b4d4",
+    sensorvalue: "150"
+}];
+
+data.forEach(function(elemnt) {
+    SensorData.insert(elemnt);
+})
