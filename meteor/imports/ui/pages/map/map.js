260545b0517a4b0aa7f2e41ebb5ec09777342497
Matth96 <Matthias.Dolstra@hva.nl>
2016-04-28 13:16:43 +0200

    add cycle icon to map insted of bleu dot

--- meteor/imports/ui/pages/map/map.js
+++ meteor/imports/ui/pages/map/map.js
@@ -11,64 +11,46 @@ Template.map.onRendered(function() {
     var map = L.map('map', {
         center: [52.376956, 4.902756],
         maxZoom: 20,
-        //minZoom: 14,
+        minZoom: 14,
         zoom: 14,
         zoomControl: false,
         attributionControl: false,
-        // maxBounds: [
-        //     [52.345197800248926, 4.82025146484375],
-        //     [52.424825961602764, 4.967708587646484]
-        // ]
+        maxBounds: [
+            [52.345197800248926, 4.82025146484375],
+            [52.424825961602764, 4.967708587646484]
+        ]
     });
-
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-        attribution: 'Project 3',
-    }).addTo(map);
 
     var sensorIcon = L.divIcon({
         className: 'sensor-icon'
     });
-    var cycleIcon = L.divIcon({
-        className: 'cycle-icon'
+    var cycleIcon = L.icon({
+        iconUrl: '/img/cycle.svg',
+        iconSize: [38, 95]
     });
 
     var cycleMarker = L.marker([52.36632373281241, 4.912347793579102], {
         icon: cycleIcon
     }).addTo(map);
 
-    // var markers = new Array();
-    // sensorPoints.forEach(function(element, index) {
-    //     var maker = L.marker([element.lat, element.lon], {
-    //         icon: sensorIcon,
-    //         data: element.sensorId
-    //     }).addTo(map).on('click', onClick);
-    // });
+    var markers = new Array();
 
-    var marker = new Array();
-    var items = [{
-        "lat": 52.36632373281241,
-        "lon": 4.912347793579102
-    }, {
-        "lat": 52.365956867310075,
-        "lon": 4.9140214920043945
-    }, {
-        "lat": 52.36472522512735,
-        "lon": 4.915437698364258
-    }];
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+        attribution: 'Project 3',
+    }).addTo(map);
 
     sensorPoints.forEach(function(item, i) {
         var LamMarker = new L.marker([item.lat, item.lon], {
             icon: sensorIcon,
             data: item.sensorId
         }).on('click', onClick);
-        marker.push(LamMarker);
-        map.addLayer(marker[i]);
+        markers.push(LamMarker);
+        map.addLayer(markers[i]);
     });
 
-    console.log(marker[0].options.data);
     checkCycle(cycleMarker);
-    // checkMarkers();
-    // console.log(makesr);
+
+    changecollor(markers, "3e7a9a1b-b0fb-4539-9fa4-a60c23b694a6");
 });
 
 function onClick(e) {
@@ -100,6 +82,14 @@ function checkCycle(marker) {
         changed: function(newDocument, oldDocument) {
             var newPositon = newDocument.data
             marker.setLatLng(newPositon).update();
+        }
+    });
+}
+
+function changecollor(markers, id) {
+    markers.forEach(function(marker, index) {
+        if (marker.options.data === id) {
+            markers[index].valueOf()._icon.style.backgroundColor = '#bb2e31';
         }
     });
 }
