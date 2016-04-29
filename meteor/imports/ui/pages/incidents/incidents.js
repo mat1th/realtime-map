ccb5278dcf22aad30b1594a8b4ecb67ca04e3956
Senny Kalidien <senny@directzichtbaar.nl>
2016-04-29 15:15:40 +0200

    Added date to notifications page

--- meteor/imports/ui/pages/incidents/incidents.js
+++ meteor/imports/ui/pages/incidents/incidents.js
@@ -3,5 +3,10 @@ import './incidents.html';
 Template.incidents.helpers({
     'data': function() {
         return Incidents.find({}).fetch({});
+    },
+    'newDate': function(date) {
+        var newDate =  moment(date, "YYYY-MM-DD HH:mm:ss").fromNow();
+
+        return newDate;
     }
 })
